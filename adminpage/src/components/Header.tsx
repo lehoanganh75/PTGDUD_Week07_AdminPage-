4499e25c45d91846e0bd12589211dd936c9433be
lehoanganh75 <gaokun2021@gmail.com>
2025-04-13 21:27:19 +0700

    10. Css Layout all admin app commit

--- adminpage/src/components/Header.tsx
+++ adminpage/src/components/Header.tsx
@@ -6,7 +6,7 @@ const Header = ({ className }) => {
         <h2 className='font-bold text-pink-400 text-3xl'>DashBoard</h2>
         <div className='flex gap-2'>
           <img src="./image/Search.png" className='h-[20px] mr-2 mt-2 absolute' alt="Search" />
-          <input type="text" placeholder='Search...' className='bg-gray-50 px-8 w-[200px] m-1 rounded-lg'/>
+          <input type="text" placeholder='Search....' className='bg-gray-50 px-8 w-[200px] m-1 rounded-lg'/>
           <button className='hover:cursor-pointer'><img src="./image/Bell 1.png" alt="Notifications" /></button>
           <button className='hover:cursor-pointer'><img src="./image/Question 1.png" alt="Help" /></button>
           <button className='hover:cursor-pointer'><img src="./image/Avatar (1).png" alt="Profile" /></button>
